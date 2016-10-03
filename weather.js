a39d209ccbb3d2473fcbc264e4955142cf6cc537
Dani <dani.code.camp@gmail.com>
2016-10-03 19:06:53 -0400

    Create weather.js

---
+++ weather.js
@@ -0,0 +1,28 @@
+var baseURL = "http://api.openweathermap.org/data/2.5/weather?"
+var apiKey = "2ca568839db1b37c90c1d38842418e08";
+
+$(document).ready(function() {
+    
+    //Using GeoLocation
+//    $.getJSON(baseURL + "lat&lon&APPID=" + apiKey, function(json) {
+//
+//        if (navigator.geolocation) {
+//            navigator.geolocation.getCurrentPosition(function(position) {
+//                
+//                var lat = position.coords.latitude.toFixed(3);
+//
+//                var lon = position.coords.longitude.toFixed(3);
+//                $(".coords").html("latitude: " + lat + "<br>longitude: " + lon);
+//            });
+//        }
+//        
+    
+    // Code from Stack Overflow: http://stackoverflow.com/questions/35496705/get-value-from-textbox-not-working-in-javascript
+//    $('#citySubmit').click(function() {
+//        var city = $('#cityName').val();
+//        $(".city").html("City:" + city);
+//    });
+
+    
+    
+});
